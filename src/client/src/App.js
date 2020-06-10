3f7d111b704d20c4d67ddf7d1ac7ddc1452482c7
Behrooz Tabesh <btabesh@zobristinc.com>
2020-06-09 22:45:28 -0700

    clean up

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -11,37 +11,3 @@ const App = () => {
 }
 
 export default App
-
-
-
-// class App extends Component {
-
-//     constructor(props) {
-//         super(props);
-//         this.state = {loading: true, errorMessage: undefined, products: null}
-//     }
-
-//     loadProducts = (response) => {
-//         console.log('response', response)
-//         this.setState({loading: false, products: response.data});
-//     }
-
-//     errorLoading = (err) => {
-//         this.setState({loading: false, errorMessage: 'There was an error loading your products.'});
-//     }
-
-//     componentDidMount() {
-//         ProductService.getProducts(this.loadProducts, this.errorLoading)
-//     }
-
-//     componentWillMount() {
-//         axios.defaults.timeout = 10000;
-//         // axios.defaults.headers.common['Authorization'] = `Bearer ${AuthStore.getToken()}`;
-//     }
-
-//     render() {
-//         return(<h2>Teahouse</h2>)
-//         }
-// }
-
-// export default App;
